1bcff615d2a76cfc771db7feaa558fb24665d624
Hugo Wetterberg <hugo.wetterberg@goodold.se>
2009-02-02 14:41:52 +0100

    We are now properly detecting if the form has a position field in the edit js.

--- js/edit.js
+++ js/edit.js
@@ -36,7 +36,7 @@ if (google && google.load) {
       return def;
     };
     
-    jQuery('#edit-simple-geo-position-wrapper').hide();
+    var has_position = jQuery('#edit-simple-geo-position-wrapper').hide().length;
     var has_area = jQuery('#edit-simple-geo-area-wrapper').hide().length;
     
     //Create map view
@@ -112,7 +112,9 @@ if (google && google.load) {
     });
     
     //Add the marker to the map
-    map.addOverlay(marker);
+    if (has_position) {
+      map.addOverlay(marker);
+    }
     
     if (has_area) {
       //Create the polygon and set it up so that the user can edit it
